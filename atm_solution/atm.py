5987c3289b0bcf064c1697171a492524e9dd04f7
Aroui Tarek <aroui94t@gmail.com>
2018-09-21 16:42:56 +0100

    update same line in files and add methods

--- atm_solution/atm.py
+++ atm_solution/atm.py
@@ -1,50 +1,30 @@
-
-
-# This part is for Workshop 8:
-# Start code Workshop 8:
-def withdraw(balance, request):
-    list_order = []
-    list_paper = [200, 100, 50, 10, 5, 2, 1]
-    index = 0
-    if request > balance or request <= 0:
-        print """
-                    There is a problem , \n
-                    that the amount entered may be greater than the total amount,\n
-                    or the amount entered will take a negative number
-              """
-    else:
-        while index < len(list_paper):
-            while request >= list_paper[index]:
-                request -= list_paper[index]
-                list_order.append(list_paper[index])
-            index+=1
-
-        for count in list_paper:
-            if list_order.count(count) >= 1:
-                print "give " + str(count)+"$ ===> number paper : " + str(list_order.count(count))
-    return "Current balance :" + str(balance - sum(list_order))
-
-print withdraw(740,424)
-# End code Workshop 8
-
-#========================================================================================================
-
-
-# This part is for Workshop 10:
-# Start code Workshop 10:
+# This part is for Workshop 12:
+# Start code Workshop 12:
 
 """ 
 Define class ATM :
-        methods => withdraw
-
+        
+        methods:
+                pablic:
+                  ==> withdraw
+                  ==> display_withdrawals
+                private:
+                  ==> __add_in_withdrawals
+                  ==> __process_request  
+ 
 """
 class ATM :
 
     def __init__(self,name_bank,balance):
         self.bank    =  name_bank
         self.balance =  balance
+        self.__withdrawals_dict = {}
 
-    def process_request(self,request):
+    def __add_in_withdrawals(self,paper,count):
+    	  self.__withdrawals_dict[paper]:count    
+
+    
+    def __process_request(self,request):
         list_order = []
         list_paper = [200, 100, 50, 10, 5, 2, 1]
         index = 0
@@ -56,11 +36,15 @@ class ATM :
 
             for count in list_paper:
                 if list_order.count(count) >= 1:
+                	  self.__add_in_withdrawals(count,list_order.count(count))	
                     print "give " + str(count)+"$ ===> number paper : " + str(list_order.count(count))
+            
             print "="*len("Current balance :" + str(self.balance - sum(list_order)))
             print "Current balance :" + str(self.balance - sum(list_order))
             print "="*len("Current balance :" + str(self.balance - sum(list_order)))
     
+    
+
     def withdraw(self,request):
 
         print "="*len("Current balance = " + str(self.balance))
@@ -75,9 +59,20 @@ class ATM :
                     or the amount entered will take a negative number
                   """
         else:
-            self.process_request(request)
-            
-# End code Workshop 10:        
+            self.__process_request(request)
+      
+    def display_withdrawals(self):
+        if len(self.__withdrawals_dict) == 0:
+            print "There is no extract amount"
+        else:
+            s = (key*value for key,value in self.__withdrawals_dict.items())
+            print "Amount extracted : " + str(sum(s))
+            print "Papers extracted :"
+            for paper, count in self.__withdrawals_dict.items():
+                print "paper ==> :" + str(paper), "count ==> :" + str(count)
+
+
+# End code Workshop 12        
 # if want how to create objects , go to file main.py         
 
 
--- atm_solution/main.py
+++ atm_solution/main.py
@@ -2,8 +2,12 @@ from atm import ATM
 
 atm1 = ATM("Bader",800)
 atm1.withdraw(452)
+atm1.display_withdrawals()
 
 atm2 = ATM("Golf",975)
 atm2.withdraw(642)
+atm2.display_withdrawals()
+
 atm2.withdraw(1000)
+atm1.display_withdrawals()
 
